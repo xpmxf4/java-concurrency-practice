2017b83ed815bf5c3e319ef5e6f81f79b390163a
dev-chpark <xpmxf5@gmail.com>
2025-01-07 21:33:52 +0900

    [ExecutorService API] 스레드 풀 중단 및 종료 - 2 shutdown() 연습중,,,

---
+++ src/main/java/org/example/chapter10/exam07/ShutdownExample.java
@@ -0,0 +1,35 @@
+package org.example.chapter10.exam07;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ShutdownExample {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++) {
+
+            executorService.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("======================="+"인터런트 걸림");
+                }
+                return 42;
+            });
+        }
+
+        executorService.shutdown();
+
+        try {
+            if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+                System.out.println("스레드 풀 강제종료.");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
