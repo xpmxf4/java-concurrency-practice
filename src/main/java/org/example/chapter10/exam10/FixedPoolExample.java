d10674132eef321261a67b4060646659d89839af
dev-chpark <xpmxf5@gmail.com>
2025-01-13 21:58:11 +0900

    [Executors] 고정 스레드풀 테스트 without ThreadFactory

---
+++ src/main/java/org/example/chapter10/exam10/FixedPoolExample.java
@@ -0,0 +1,33 @@
+package org.example.chapter10.exam10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FixedPoolExample {
+    private static final int NUM_OF_THREADS = 5;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        List<Future<String>> futures = new ArrayList<>();
+
+        for (int i = 0; i < NUM_OF_THREADS; i++) {
+            Future<String> future = executorService.submit(() -> {
+                Thread.sleep(1000);
+                return "Hello World - " + Thread.currentThread().getName();
+            });
+            futures.add(future);
+        }
+
+        for (int i = 0; i < NUM_OF_THREADS; i++) {
+            String result = futures.get(i).get();
+            System.out.println("result = " + result);
+        }
+
+        executorService.shutdown();
+    }
+}
